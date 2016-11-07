2209bfab94ce430cb39b0aaf382888dc4db6009d
Hendrikto <hendrik.to@gmail.com>
2016-11-07 17:53:49 +0100

    Add a struct to represent Sudokus.

--- src/Main.c
+++ src/Main.c
@@ -13,6 +13,13 @@ struct Cell {
 	uint16_t *block;
 };
 
+struct Sudoku {
+	struct Cell cells[81];
+	uint16_t rows[9];
+	uint16_t columns[9];
+	uint16_t blocks[9];
+};
+
 int main(int argc, char** argv) {
 	return EXIT_SUCCESS;
 }
