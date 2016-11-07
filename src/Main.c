d845ab5d6379fba44758c2db4cb287b4859ddb7b
Hendrikto <hendrik.to@gmail.com>
2016-11-07 01:09:19 +0100

    Add a struct to represent Sudoku cells.

--- src/Main.c
+++ src/Main.c
@@ -4,6 +4,14 @@
  * @author: Hendrik Werner
  */
 
+// a cell of the Sudoku
+struct Cell {
+	char value;
+	int16_t *row;
+	int16_t *column;
+	int16_t *block;
+};
+
 int main(int argc, char** argv) {
 	return EXIT_SUCCESS;
 }
